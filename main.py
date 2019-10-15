55432c04066a244c9a9ec3bfc88654db3b8b31ff
m12watanabe1a <m12watanabe1a@gmail.com>
2019-10-15 22:40:40 +0900

    declare getStartPotin function

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ def readCsv(filename):
     df = pd.read_csv(path + filename, index_col=0)
     return df
 
-def plotCsv(df):
-    df.plot();
-
 def getCalibrationValue(df):
 
     target_values = df["Analog2[V]"]
@@ -27,9 +24,28 @@ def getCalibrationValue(df):
             break
     return sum(stack_values) / len(stack_values)
 
+def getStartPoint(df):
+    times = df.index.values.tolist()
+    analog1 = df["Analog1[V]"]
+    analog2 = df["Analog2[V]"]
+    analog9 = df["Analog9[V]"]
+    for (time, val1, val2, val3) in zip(times, analog1, analog2, analog9):
+        if((val1 > 0.0) and (val2 > 0.0) and (val3 > 0.0)):
+            start = time
+            init_val1 = val1
+            init_val2 = val2
+            init_val3 = val3
+            break
+    df["Analog1[V]"] -= init_val1
+    df["Analog2[V]"] -= init_val2
+    df["Analog9[V]"] -= init_val3
+    return df[start:]
+
 if __name__ == "__main__":
     filename = "PET.csv"
     df = readCsv(filename)
-    plotCsv(df)
     epsilon_ratio = getCalibrationValue(df)
-    print(epsilon_ratio)
+    tensile_value = getStartPoint(df)
+
+    tensile_value.plot()
+    plt.show()
